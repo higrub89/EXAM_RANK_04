20727ba00c1a8ae3b6c38d2d09fc5dbf92b43c40
Ruben Dario Higuita Rubio <higuitaruben@hotmail.com>
2025-11-27 21:47:38 +0100

    Ejercicio ft_popen

---
+++ level_1/ft_popen/ft_popen.c
@@ -0,0 +1,48 @@
+include <unistd.h>
+
+int ft_popen(const char *file, char *const char av[], char type)
+{
+    pid_t   pid;
+    int     fd[2];
+    if (!file || !av || (type != 'r' && type != 'w'))
+        return -1;
+    if (pipe(fd) == -1)
+        return -1;
+    pid = fork();
+    if (pid == -1)
+    {
+        close(fd[0]);
+        close(fd[1]);
+        return -1; 
+    }
+    if (pid == 0)
+    {
+        if (type == 'r')
+        {
+            if (dup2(fd[1], STDIN_FILENO) == -1)
+                exit(1);
+        }
+        else
+        {
+            if (dup2(fd[0], STDIN_FILENO) == -1)
+                exit(1);
+        }
+        close(fd[0]);
+        close(fd[1]);
+        execvp(file, av);
+        exit(1);
+    }
+    else
+    {
+        if (type == 'r')
+        {
+            close(fd[1]);
+            return (fd[0]);
+        }
+        else
+        {
+            close(fd[0]);
+            return(fd[1]);
+        }
+    }
+}
