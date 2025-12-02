d569ce12e5d89ae5c30e87ae6588d0cf536343c1
Ruben Dario Higuita Rubio <higuitaruben@hotmail.com>
2025-12-02 02:12:03 +0100

    EJERCICIOS PICOSHELL

--- level_1/ft_popen/ft_popen.c
+++ level_1/ft_popen/ft_popen.c
@@ -7,34 +7,34 @@ int ft_popen(const char *file, char *const argv[], char type)
     int     fd[2];
 
     if (!file || !argv || (type != 'r' && type != 'w'))
-        return (-1);
+        return -1;
     if (pipe(fd) == -1)
-        return (-1);
+        return -1;
     pid = fork();
-    if (pid == -1)
-    {
-        close(fd[0]);
-        close(fd[1]);
-        return (-1);
-    }
     if (pid == 0)
     {
         if (type == 'r')
         {
+            if (dup2(fd[1], STDOUT_FILENO) == -1)
+            {
+                close(fd[0]);
+                close(fd[1]);
+                exit (1);
+            }
+            else
+            {
+                if (dup2(fd[0], STDIN_FILENO) == -1)
+                {
+                    close(fd[0]);
+                    close(fd[1]);
+                    exit(1);
+                }
+            }
             close(fd[0]);
             close(fd[1]);
-            exit (1);
+            execvp(file, argv);
+            exit(1);
         }
-        else
-        {
-            close(fd[0]);
-            close(fd[1]);
-            exit (1);
-        }
-        close(fd[0]);
-        close(fd[1]);
-        execvp(file, argv);
-        exit(1);
     }
     else
     {
