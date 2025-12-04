72b05c32dc051d347d989c481138a4c628e6b5d2
Ruben Dario Higuita Rubio <higuitaruben@hotmail.com>
2025-12-04 08:22:14 +0100

    EXAM READY...

--- level_1/picoshell/picoshell_mio.c
+++ level_1/picoshell/picoshell_mio.c
@@ -9,7 +9,7 @@ int picoshell(char **cmds[])
     int     last_fd = -1;
     int     i = 0;
 
-    while(cmds[i])
+    while (cmds[i])
     {
         if (cmds[i + 1] && pipe(fd) == -1)
             return 1;
@@ -21,7 +21,7 @@ int picoshell(char **cmds[])
                 close(fd[0]);
                 close(fd[1]);
             }
-            return (1);
+            return 1;
         }
         if (pid == 0)
         {
@@ -34,7 +34,7 @@ int picoshell(char **cmds[])
             if (cmds[i + 1])
             {
                 close(fd[0]);
-                if (dup2(fd[1], STDOUT_FILENO) == -1)
+                if (fd[1], STDOUT_FILENO) == -1)
                     exit(1);
                 close(fd[1]);
             }
@@ -48,11 +48,11 @@ int picoshell(char **cmds[])
             close(fd[1]);
             last_fd = fd[0];
         }
-        i++;       
+        i++;
     }
-    while (wait(NULL) > 0)
+    while(wait(NULL) > 0)
     {
         ;
     }
-    return (0);
+    return 0;
 }
